7dc15b012fc0c8460f06fad2b7d2283d60adc2e7
Daniel Butler <daniel@kefa.uk>
2024-05-10 23:02:33 +0100

    added .gitignore to ignore generated mikrotik config files

--- backup_router_config.sh
+++ backup_router_config.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Script to create backup of Mikrotik config with datestamp and copy to
+# local workstation. Prompts for comment to describe latest change.
+
 DATE=$(date +"%Y%m%d-%H%M")
 read -p "Enter short comment using letters and hyphens only: " COMMENT
 FILE="mikrotik-${DATE}-${COMMENT}.rsc"
