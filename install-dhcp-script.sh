a2ec265b47e816d55c6851e05ef68f76ed92e85b
Daniel Butler <daniel@kefa.uk>
2024-05-11 00:27:39 +0100

    corrected file format and updated comments

--- install-dhcp-script.sh
+++ install-dhcp-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Script uses ssh to copy the same dhcp lease-script to each of the dhcp servers configured on the Mikrotik
+# Installs dhcp-managed-dns-records.rsc to multiple dhcp servers lease-script on Mikrotik via SSH
 # Script needs to escape out newlines, double quotes and dollar signs to work...but it also needs
 # to preserve '\n' newline characters used inside the scripts.
 
 DHCP_SERVERS=$(ssh admin@router.lan '/ip/dhcp-server/print proplist="name"' | grep -v NAME | awk -F' ' '{print $2}' | tr '\n\r' '\n')
-SCRIPT=$(cat dhcp-managed-dns-records.sh | sed 's/\\n/\\\\n/g' | sed 's/$/\\n/g' | sed 's/\(["$]\)/\\\1/g' | tr -d '\n')
+SCRIPT=$(cat dhcp-managed-dns-records.rsc | sed 's/\\n/\\\\n/g' | sed 's/$/\\n/g' | sed 's/\(["$]\)/\\\1/g' | tr -d '\n')
 
 for DHCP_SERVER in $DHCP_SERVERS; do
    echo $DHCP_SERVER
