f03b17785b146e299a75760ed8f1bbad2ff3e6d1
Daniel Butler <daniel@kefa.uk>
2024-05-10 23:33:09 +0100

    added updated comment in install script

--- install-dhcp-script.sh
+++ install-dhcp-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Script uses ssh to copy the same dhcp lease-script to each of the dhcp servers configured on the Mikrotik
-# Script needs to escape out newlines, double quotes and dollar signs to work
+# Script needs to escape out newlines, double quotes and dollar signs to work...but it also needs
+# to preserve '\n' newline characters used inside the scripts.
 
 DHCP_SERVERS=$(ssh admin@router.lan '/ip/dhcp-server/print proplist="name"' | grep -v NAME | awk -F' ' '{print $2}' | tr '\n\r' '\n')
 SCRIPT=$(cat dhcp-managed-dns-records.sh | sed 's/\\n/\\\\n/g' | sed 's/$/\\n/g' | sed 's/\(["$]\)/\\\1/g' | tr -d '\n')
