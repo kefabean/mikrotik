17a2cfcefeff7f9019ddf0e8243b4267c6628d20
Daniel Butler <daniel@kefa.uk>
2025-09-17 08:13:18 +0100

    updated dns name for router.bridge

--- backup_router_config.sh
+++ backup_router_config.sh
@@ -6,5 +6,5 @@
 DATE=$(date +"%Y%m%d-%H%M")
 read -p "Enter short comment using letters and hyphens only: " COMMENT
 FILE="mikrotik-${DATE}-${COMMENT}.rsc"
-ssh admin@router.lan "export file=${FILE}"
-scp admin@router.lan:~/$FILE .
+ssh admin@router.bridge "export file=${FILE}"
+scp admin@router.bridge:~/$FILE .
